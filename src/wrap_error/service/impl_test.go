249d9c2ebcd60b8ca5673461296b3fbddc1601f3
shaoyongzhen <syztobenum1@163.com>
2021-06-07 10:40:18 +0800

    add success ut

--- src/wrap_error/service/impl_test.go
+++ src/wrap_error/service/impl_test.go
@@ -24,4 +24,8 @@ func TestLogin(t *testing.T) {
 	assert.Equal(t, false, errors.Is(err, dao.SqlErrNoRows))
 	assert.Equal(t, "wrong password", fmt.Sprintf("%v", errors.Cause(err)))
 	assert.Equal(t, "Login Failed: wrong password", err.Error())
+
+	// login success
+	err = s.Login("Eva", "pwd")
+	assert.Equal(t, nil, err)
 }
